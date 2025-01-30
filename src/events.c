7064569abd7fb9149fc019879645216c2168079a
Alexandre Harder <aharder@c3r2s1.42luxembourg.lu>
2025-01-30 16:18:08 +0100

    corrected norm errors

--- src/events.c
+++ src/events.c
@@ -6,68 +6,58 @@
 /*   By: aharder <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 15:21:16 by aharder           #+#    #+#             */
-/*   Updated: 2024/12/20 16:44:11 by aharder          ###   ########.fr       */
+/*   Updated: 2025/01/30 16:14:21 by aharder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdfheader.h"
 
+void	free_and_exit(t_vars *vars)
+{
+	mlx_destroy_image(vars->mlx, vars->img);
+	mlx_destroy_window(vars->mlx, vars->win);
+	mlx_destroy_display(vars->mlx);
+	free(vars->mlx);
+	free_map(vars->map);
+	exit(0);
+}
+
+void	zoom(t_vars *vars, int x)
+{
+	vars->size += x;
+	ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
+	generategrid(vars, vars->map, vars->size);
+}
+
+void	horizontal_move(t_vars *vars, int x)
+{
+	vars->defaulty += x;
+	ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
+	generategrid(vars, vars->map, vars->size);
+}
+
+void	vertical_move(t_vars *vars, int x)
+{
+	vars->defaultx + x;
+	ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
+	generategrid(vars, vars->map, vars->size);
+}
+
 int	key_hook(int keycode, t_vars *vars)
 {
-	printf("Keycode : %d\n", keycode);
 	if (keycode == 65307)
-	{
-		mlx_destroy_image(vars->mlx, vars->img);
-		mlx_destroy_window(vars->mlx, vars->win);
-		mlx_destroy_display(vars->mlx);
-		free(vars->mlx);
-		free_map(vars->map);
-		exit(0);
-	}
+		free_and_exit(vars);
 	if (keycode == 65362)
-	{
-		vars->size += 1;
-		ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
-		generategrid(vars, vars->map, vars->size);
-	}
+		zoom(vars, 1);
 	if (keycode == 65364)
-	{
-		vars->size -= 1;
-		ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
-		generategrid(vars, vars->map, vars->size);
-	}
+		zoom(vars, -1);
 	if (keycode == 113)
-	{
-		vars->defaultx += 5;
-		ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
-		generategrid(vars, vars->map, vars->size);
-	}
+		vertical_move(vars, 5);
 	if (keycode == 100)
-	{
-		vars->defaultx -= 5;
-		ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
-		generategrid(vars, vars->map, vars->size);
-	}
+		vertical_move(vars, -5);
 	if (keycode == 122)
-	{
-		vars->defaulty += 5;
-		ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
-		generategrid(vars, vars->map, vars->size);
-	}
+		horizontal_move(vars, 5);
 	if (keycode == 115)
-	{
-		vars->defaulty -= 5;
-		ft_bzero(vars->addr, 1920 * 1080 *(vars->bpp / 8));
-		generategrid(vars, vars->map, vars->size);
-	}
+		horizontal_move(vars, -5);
 	return (0);
 }
-/*
-int mouse_event(int button, int x, int y, void *param)
-{
-    t_vars *tutorial = param;
-    (void)button;
-    mlx_pixel_put(tutorial->mlx, tutorial->win, x, y, 0xFFFFFF);
-    
-    return (1);
-}*/
